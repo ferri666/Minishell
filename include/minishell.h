a02319ffaea55e9954b46ed7bf2b8a8176578d90
ffons-ti <ffons-ti@student.42madrid.com>
2023-09-12 11:14:55 +0200

    MAXPATHLEN

--- include/minishell.h
+++ include/minishell.h
@@ -6,12 +6,15 @@
 /*   By: ffons-ti <ffons-ti@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 18:41:04 by ffons-ti          #+#    #+#             */
-/*   Updated: 2023/08/30 14:01:08 by ffons-ti         ###   ########.fr       */
+/*   Updated: 2023/09/08 17:58:58 by ffons-ti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# ifndef MAXPATHLEN
+#  define MAXPATHLEN=1024
+# endif
 
 # include <unistd.h>
 # include <stdio.h>
